1d9995c1a850e0fe927cff9f8ba05598e7cd2a34
Rabee Aljaramani <77026472+rabeexxxx@users.noreply.github.com>
2022-06-26 11:25:04 +0400

    Update Home.tsx

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -2,6 +2,9 @@ import gsap from 'gsap';
 import React, { useEffect } from 'react';
 import me from '../images/me.png';
 export default function Home() {
+  const click = () => {
+    gsap.from('.home-cont', { marginLeft: -1000 });
+  };
   useEffect(() => {}, []);
 
   return (
@@ -9,7 +12,9 @@ export default function Home() {
       <div className="flex-row info-img-cont">
         <div className="my-name-roles">
           <h5 className="my-this-me">Hey, This is me</h5>
-          <h1 className="my-name">Rabee</h1>
+          <h1 className="my-name" onClick={() => click()}>
+            Rabee
+          </h1>
           <h1 className="my-web">Web</h1>
           <h1 className="my-fornt-end">Front-End</h1>
           <h1 className="my-developer">DEVELOPER</h1>
