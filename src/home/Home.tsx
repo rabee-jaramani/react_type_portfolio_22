fe8d826fbf35e506f1e56f366b4a0fa9f6d1f899
Rabee Aljaramani <77026472+rabeexxxx@users.noreply.github.com>
2022-06-26 11:11:28 +0400

    start

---
+++ src/home/Home.tsx
@@ -0,0 +1,24 @@
+import gsap from 'gsap';
+import React, { useEffect } from 'react';
+import me from '../images/me.png';
+export default function Home() {
+  useEffect(() => {}, []);
+
+  return (
+    <div className="container home-cont">
+      <div className="flex-row info-img-cont">
+        <div className="my-name-roles">
+          <h5 className="my-this-me">Hey, This is me</h5>
+          <h1 className="my-name">Rabee</h1>
+          <h1 className="my-web">Web</h1>
+          <h1 className="my-fornt-end">Front-End</h1>
+          <h1 className="my-developer">DEVELOPER</h1>
+        </div>
+        <div className="my-img-cont flex-row">
+          <img src={me} alt="rabee" />
+        </div>
+      </div>
+      {/* <div className="base"></div> */}
+    </div>
+  );
+}
